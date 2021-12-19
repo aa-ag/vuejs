db864988ed83b921051feb4317d8055e35bbf0e9
aa-ag <aaron.aguerrevere@gmail.com>
2021-12-19 17:21:06 -0600

    added form for users to leave reviews

---
+++ components/ReviewForm.js
@@ -0,0 +1,24 @@
+app.component('review-form', {
+    template:
+    /*html*/
+    `<form class="review-form">
+    <h3>Leave a review</h3>
+    <label for="name">Name:</label>
+    <input id="name">
+
+    <label for="review">Review:</label>
+    <textarea id="review"></textarea>
+
+    <label for="rating">RAting:</label>
+    <select id="rating">
+      <option>😡</option>
+      <option>😐</option>
+      <option>🙂</option>
+      <option>🤩</option>
+    </select>
+
+    <input class="button" type="submit" value="Submit">
+    </form>
+    `
+})
+
