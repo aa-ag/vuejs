b926aa386289a311c1f02bb0b9338caede352461
aa-ag <aaron.aguerrevere@gmail.com>
2021-12-19 10:18:42 -0600

    added conditional rendering logic in html using v-if, v-else & updated inStock variable in main.js to test change: working as expected

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ const app = Vue.createApp({
         return {
             product: 'Socks',
             image: './assets/images/socks_green.jpg',
-            inStock: true,
+            inStock: false,
             url: 'http://aguerrevere.dev'
         }
     }
