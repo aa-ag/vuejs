b7b227eee5e794b9730d48a3af0adcdb63e3fb16
aa-ag <aaron.aguerrevere@gmail.com>
2021-12-19 13:54:43 -0600

    added cart button in frontend & logic into data object in main.js

--- main.js
+++ main.js
@@ -1,11 +1,16 @@
 const app = Vue.createApp({
     data() {
         return {
+            cart: 0,
             product: 'Socks',
             image: './assets/images/socks_green.jpg',
             inventory: 9,
             onSale: false,
             details: ['50% cotton', '35% wool', '20% polyester'],
+            options: [
+                {id: 1, color: 'green'},
+                {id: 2, color: 'blue'},
+            ],
             url: 'http://aguerrevere.dev'
         }
     }
