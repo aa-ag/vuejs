200f80702fa1c3fc4f04fa6360982e6d938b322d
aa-ag <aaron.aguerrevere@gmail.com>
2021-12-18 23:08:59 -0600

    refactored main.js & index.html to display navbar, product image, and reference image from assets

--- main.js
+++ main.js
@@ -2,8 +2,7 @@ const app = Vue.createApp({
     data() {
         return {
             product: 'Socks',
-            price: '$10',
-            description: 'they keep your feet warm!'
+            image: './assets/images/socks_green.jpg'
         }
     }
 })
