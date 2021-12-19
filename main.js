29060810da5531aebaece4534650fa27755a88e4
aa-ag <aaron.aguerrevere@gmail.com>
2021-12-19 13:43:58 -0600

    added list rendering logic bot in main.js & html: list in details, and v-for in index.html

--- main.js
+++ main.js
@@ -3,8 +3,9 @@ const app = Vue.createApp({
         return {
             product: 'Socks',
             image: './assets/images/socks_green.jpg',
-            inventory: 10,
+            inventory: 9,
             onSale: false,
+            details: ['50% cotton', '35% wool', '20% polyester'],
             url: 'http://aguerrevere.dev'
         }
     }
