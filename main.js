9813787b03b1bda4e98d81fefcec4ea0bf926064
aa-ag <aaron.aguerrevere@gmail.com>
2021-12-19 14:45:26 -0600

    refactored code to let user have current info on each color, and not just inventory in general

--- main.js
+++ main.js
@@ -4,13 +4,19 @@ const app = Vue.createApp({
             cart: 0,
             product: 'Socks',
             brand: 'Warm&Comfy',
-            image: './assets/images/socks_green.jpg',
+            selectedOption: 0,
             inventory: 11,
             onSale: false,
             details: ['50% cotton', '35% wool', '20% polyester'],
             options: [
-                {id: 1, color: 'green', image: './assets/images/socks_green.jpg'},
-                {id: 2, color: 'blue', image: './assets/images/socks_blue.jpg'},
+                {id: 1, color: 'green', 
+                image: './assets/images/socks_green.jpg',
+                quantity: 11
+                },
+                {id: 2, color: 'blue', 
+                image: './assets/images/socks_blue.jpg',
+                quantity: 0
+                },
             ],
             url: 'http://aguerrevere.dev'
         }
@@ -26,13 +32,20 @@ const app = Vue.createApp({
                 this.inventory += 1
             }
         }, 
-        updateImage(optionImage) {
-            this.image = optionImage
+        updateOption(index) {
+            this.selectedOption = index
+            console.log(index)
         }
     },
     computed: {
         title() {
             return this.brand + ' ' + this.product
+        },
+        image() {
+            return this.options[this.selectedOption].image
+        },
+        inStock() {
+            return this.options[this.selectedOption].quantity
         }
     }
 })
